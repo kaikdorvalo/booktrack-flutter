5be69eb95365e762c007353912ffd5ca20884ed1
kaikdorvalo <kaikdorvalo@gmail.com>
2024-10-12 18:03:37 -0300

    feat: add pages to nav

--- lib/screens/default/default.dart
+++ lib/screens/default/default.dart
@@ -1,13 +1,43 @@
 import 'package:booktrack_flutter/constants.dart';
+import 'package:booktrack_flutter/screens/home/pages/home.dart';
 import 'package:booktrack_flutter/widgets/nav_bottom/nav_bottom.dart';
 import 'package:flutter/material.dart';
 
-class Default extends StatelessWidget {
+class Default extends StatefulWidget {
+  Default({super.key});
+
+  @override
+  _DefaultState createState() => _DefaultState();
+}
+
+class _DefaultState extends State<Default> {
+  int selectedIndex = 0;
+  List<Widget> _screens = [];
+
+  void changeIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      Home(),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _screens.elementAt(selectedIndex),
       backgroundColor: primary,
-      bottomNavigationBar: NavBottom(),
+      bottomNavigationBar: NavBottom(
+        selectedIndex: selectedIndex,
+        screens: _screens,
+        changePage: changeIndex,
+      ),
     );
   }
 }
